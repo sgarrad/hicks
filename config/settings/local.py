08385946dec19c871ab932313efb71e3ba1cc3b2
Steven Garrad <sgarrad@users.noreply.github.com>
2015-10-04 15:44:03 +0100

    Adding support for django-debug-toolbar
    Refactored the requirements.txt to base.txt and local.txt
    Django-debug-toolbar is installed under local only
    Also added coverage into local. This is for local testing, travis ci
    explicitly installs coverage during testing, with base requirements.

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,3 +7,8 @@ from .common import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# django-debug-toolbar: https://github.com/django-debug-toolbar/django-debug-toolbar
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
+
