005c0baabce92b247ee49957997a74d80e5bb87a
Steven Garrad <sgarrad@users.noreply.github.com>
2015-10-05 23:11:06 +0100

    Adding initial support for language management
    Created hicks_language app
    Registered app in app settings
    Created basic language model with language code and display name fields
    Registered Language model for admin view

---
+++ hicks/hicks_language/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Language(models.Model):
+    code = models.CharField(max_length=50,
+                            null=False,
+                            unique=True,
+                            verbose_name='Code',
+                            help_text='http://www.w3.org/International/articles/language-tags')
+    display_name = models.CharField(max_length=255,
+                                    null=False,
+                                    verbose_name='Display Name')
+
+    class Meta:
+        ordering = ['code']
+
+    def __str__(self):
+        return "{0} - {1}".format(self.display_name, self.code)
