a1698d5f71cd3a57f740d98e7da7209713ad962b
Eamonn Lawlor <lawlor@users.noreply.github.com>
2015-10-13 20:59:55 +0100

    Fix broken redirect when submitting upload form Form now checks that both fields are completed and redirects to same page if successful.

--- hicks/hicks_import_export/views.py
+++ hicks/hicks_import_export/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
 from .forms import UploadFileForm
 
 # Create your views here.
@@ -7,11 +9,12 @@ from .forms import UploadFileForm
 #def import_view(request):
     #return render(request, 'hicks_import_export/import.html')
 
+@csrf_protect
 def import_view(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            return HttpResponseRedirect('/success/url/')
+            return HttpResponseRedirect('.')
     else:
         form = UploadFileForm()
-    return render_to_response('hicks_import_export/import.html', {'form': form})
+    return render_to_response('hicks_import_export/import.html', {'form': form}, RequestContext(request))
